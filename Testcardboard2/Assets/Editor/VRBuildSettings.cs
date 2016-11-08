f7410e907713ed96d0d174ab67e43863e834fc4e
jinhyeon <wlsgussla123@gmail.com>
2016-11-08 15:32:21 -0500

    test

---
+++ Testcardboard2/Assets/Editor/VRBuildSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class VRBuildSettings {
+    [MenuItem("VR Build Settings/Build GearVR")]
+    static void BuildGearVRSettings()
+    {
+        PlayerSettings.virtualRealitySupported = true;
+        GvrViewer cardboard = (GvrViewer)GameObject.FindGameObjectWithTag("Player").GetComponent<GvrViewer>();
+        GvrHead carboardHead = cardboard.gameObject.transform.GetChild(0).GetComponent<GvrHead>();
+        
+    }
+}
